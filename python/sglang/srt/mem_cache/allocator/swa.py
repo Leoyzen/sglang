1fd173b2b357f24fee3a41c5ce6b6b6169cfc283
Leon Gao <leon.gao19@gmail.com>
2026-08-08 03:34:44 -0700

    [Bugfix] Fix batched KV free aliasing (#34067)

--- python/sglang/srt/mem_cache/allocator/swa.py
+++ python/sglang/srt/mem_cache/allocator/swa.py
@@ -324,7 +324,7 @@ class SWATokenToKVPoolAllocator(BaseTokenToKVPoolAllocator):
             self.full_attn_allocator.free(free_index)
             self.free_swa(free_index)
         else:
-            self.free_group.append(free_index)
+            self.free_group.append(self._copy_for_free_group(free_index))
         assert (
             self.full_attn_allocator.available_size() <= self.full_attn_allocator.size
         )
@@ -483,13 +483,16 @@ class PureSWATokenToKVPoolAllocator(SWATokenToKVPoolAllocator):
         if self.is_not_in_free_group:
             self.swa_attn_allocator.free(free_index[free_index > 0])
         else:
-            self.free_group.append(free_index)
+            self.free_group.append(self._copy_for_free_group(free_index))
         assert self.swa_attn_allocator.available_size() <= self.swa_attn_allocator.size
 
     def free_swa(self, free_index: torch.Tensor):
         if free_index.numel() == 0:
             return
-        self.swa_attn_allocator.free(free_index[free_index > 0])
+        if self.is_not_in_free_group:
+            self.swa_attn_allocator.free(free_index[free_index > 0])
+        else:
+            self.free_group.append(self._copy_for_free_group(free_index))
 
     def free_group_begin(self):
         self.is_not_in_free_group = False
